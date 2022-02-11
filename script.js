5b9240043f31a9f7ea87ec3584cd4f3ad7fd552f
仲川舞 <barairopop0319@gmail.com>
2022-02-11 22:10:09 +0900

    update:Change variable name.

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-var targetElements = document.getElementById('list');
-targetElements.insertAdjacentHTML('afterbegin','<li>これです</li>');
+var targetElement = document.getElementById('list');
+targetElement.insertAdjacentHTML('afterbegin','<li>これです</li>');
