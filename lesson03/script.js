3c9116b9ae0e99f0c5b0a00fabf67dc6cfdd0077
仲川舞 <barairopop0319@gmail.com>
2022-02-16 23:53:15 +0900

    change:use "document.createDocumentFragment();" and template literals.

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,17 +1,19 @@
 const ul = document.getElementById('js-list');
-const aHref = ['a1.html','a2.html'];
-const aText = ['a1','a2'];
 const imgSrc = ['./img/bookmark.png','./img/message.png'];
 
-for (let i= 0; i < aHref.length; i++){
+const fragment = document.createDocumentFragment();
+
+for (let i= 0; i < imgSrc.length; i++){
     const li = document.createElement('li');
 
     const a = document.createElement('a');
-    a.href = aHref[i];
-    a.textContent = aText[i];
+    a.href = `a${i+1}.html`;
+    a.textContent = `a${i+1}`;
 
     const img = document.createElement('img');
     img.src = imgSrc[i];
 
-    ul.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin',img);
+    fragment.appendChild(li).appendChild(a).insertBefore(img,a.firstChild);
 }
+
+ul.appendChild(fragment);
