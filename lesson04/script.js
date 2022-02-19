aebf7b4fa50a51f5e75fc8e55595e78bd2c44cfb
仲川舞 <barairopop0319@gmail.com>
2022-02-19 16:22:56 +0900

    change:Change listContents to lists.
    To make the object name easy to understand.

--- lesson04/script.js
+++ lesson04/script.js
@@ -1,11 +1,11 @@
 const ul = document.getElementById('js-list');
-const listContents = [
+const lists = [
  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
 ]
 
 const fragment = document.createDocumentFragment();
-for(const list of listContents){
+for(const list of lists){
     const li = document.createElement('li');
     const a = document.createElement('a');
     const img = document.createElement('img');
