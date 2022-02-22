b8f47f55b409b6a39f833808da00cb72618f233d
仲川舞 <barairopop0319@gmail.com>
2022-02-23 08:21:21 +0900

    change:I put a variable declaration in a Promise.
    This is to make the code easier to read.

--- lesson06/script.js
+++ lesson06/script.js
@@ -1,9 +1,9 @@
-const lists = [
-    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-];
-
 const promise = new Promise(resolve => {
+    const lists = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ];
+    
     setTimeout(() => resolve(lists),3000);
 });
 
