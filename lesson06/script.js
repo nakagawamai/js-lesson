af187e3abf32b02d00d477d42819c8239db5360c
仲川舞 <barairopop0319@gmail.com>
2022-02-23 09:00:19 +0900

    change:Change variable name.
    Because meaningful names are easy to understand.

--- lesson06/script.js
+++ lesson06/script.js
@@ -1,16 +1,16 @@
-const promise = new Promise(resolve => {
+const createList = new Promise(resolve => {
     const lists = [
         {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
         {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
     ];
-    
+
     setTimeout(() => resolve(lists),3000);
 });
 
-promise.then((value) => {
+createList.then((value) => {
     const fragment = document.createDocumentFragment();
     for(const list of value){
-        const li = document.createElement('li');
+     const li = document.createElement('li');
         const a = document.createElement('a');
         const img = document.createElement('img');
         a.textContent = list.text;
