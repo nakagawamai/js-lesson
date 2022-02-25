db0966f5e4fda096bb9083e12fe048cdf35b2e7b
仲川舞 <barairopop0319@gmail.com>
2022-02-25 21:34:36 +0900

    fix:Put createLoading function outside Promise.
    Because createLoading should be outside of promise.

--- lesson07/script.js
+++ lesson07/script.js
@@ -27,8 +27,8 @@ function createList(value){
 }
 
 function getList(){
-    return new Promise(resolve => {  
     createLoading();
+    return new Promise(resolve => {  
     const lists = [
         {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
         {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
