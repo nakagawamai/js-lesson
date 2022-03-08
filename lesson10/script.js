b65b086dc3d2d2637ea509e48fa0d27db52a70d1
仲川舞 <barairopop0319@gmail.com>
2022-03-08 17:35:15 +0900

    feat:lesson10
    add try,catch,finally

---
+++ lesson10/script.js
@@ -0,0 +1,55 @@
+function showLoadingImg(){
+    const loadingImg = document.createElement('img');
+    loadingImg.src = "loading-circle.gif";
+    loadingImg.alt = "ローディング画像";
+    document.getElementById('js-loading').appendChild(loadingImg);
+}
+
+function removeLoadingImg(){
+    document.getElementById('js-loading').remove();
+}
+
+function getDataAfter3Seconds(){
+    return new Promise((resolve,reject) => {
+        const categriesData = [
+            {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+            {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+        ];
+        setTimeout(() => {
+            if(categriesData){
+                resolve(categriesData);
+            }else{
+                reject(new Error('データの取得ができませんでした'));
+            }
+        },3000);
+    });
+}
+
+async function getDataProcess(){
+    showLoadingImg();
+    try{
+       return await getDataAfter3Seconds();
+    }catch(e){
+        console.error(e.message);
+    }finally{
+        removeLoadingImg();
+    }
+}
+
+async function  showCategoriesList(){
+    const result = await getDataProcess();
+    const fragment = document.createDocumentFragment();
+    for(const list of result){
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+        a.textContent = list.text;
+        a.href = `/${list.to}`;
+        img.src = list.img;
+        img.alt = list.alt;
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    }
+    document.getElementById('js-list').appendChild(fragment);
+}
+
+showCategoriesList();
