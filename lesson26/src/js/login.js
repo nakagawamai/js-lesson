64b5090e2a1707efadef3af8b2aa90f1bb43f9b7
仲川舞 <barairopop0319@gmail.com>
2023-03-15 07:03:04 +0900

    refactor: Change to one line.

--- lesson26/src/js/login.js
+++ lesson26/src/js/login.js
@@ -88,10 +88,7 @@ for (const input of inputSelector){
     });
 }
 
-togglePasswordButton.addEventListener('click', () => {
-
-    togglePassword(passwordInput,togglePasswordButton);
-});
+togglePasswordButton.addEventListener('click', () => togglePassword(passwordInput,togglePasswordButton));
 
 togglePasswordButton.addEventListener('blur', (event) => {
     if(isRelatedTarget(event,"current-password")) return;
