824a40beed4863bc62993e6318e7e23d249bf450
仲川舞 <barairopop0319@gmail.com>
2023-05-06 11:23:20 +0900

    add: register page tests

--- lesson28/playwright.config.cjs
+++ lesson28/playwright.config.cjs
@@ -38,7 +38,7 @@ module.exports = defineConfig({
   ],
   webServer: {
     command: 'yarn dev',
-    url: 'http://localhost:3000/',
+    url: 'http://127.0.0.1:3000/',
     reuseExistingServer: !process.env.CI,
   },
 });
